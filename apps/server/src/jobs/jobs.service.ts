92a4a898e02bfa355195838e8cae82e583746da2
talkenigs <talkenigs@gmail.com>
2026-03-09 14:53:27 +0200

    messagses

--- apps/server/src/jobs/jobs.service.ts
+++ apps/server/src/jobs/jobs.service.ts
@@ -1,14 +1,7 @@
 
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class JobsService {
-    private readonly logger = new Logger(JobsService.name);
-
-    @Cron(CronExpression.EVERY_30_SECONDS)
-    handleCron(): void {
-        this.logger.debug('Called every 30 seconds');
-    }
 
 }
